49d182621379d123eac87cafd16e2498e122e7e6
caristopmer <chris.mcmenomy@gmail.com>
2017-12-18 12:17:22 -0600

    Solution working. Utilized a stack to check for same numbers on back half of integer.

--- runner.js
+++ runner.js
@@ -12,5 +12,41 @@ var num4 = 1233;
 
 
 var isPalindrome = function(x) {
-  
+  // convert int to array of digits
+  // utilize an array to act as a stack
+  // iterate through array with for loop, pushing to stack
+  // once halfway point (or point where palindrome should reverse) is reached, begin to pop from stack and compare to current number.
+  // if there is ever not a match, number is not a palindrome
+
+  var digitArray = x.toString().split("");
+  var digitStack = [];
+  var halfwayPoint = Math.floor(digitArray.length / 2);
+
+  if (digitArray.length % 2 === 1) {
+    for(var i = 0; i < digitArray.length; i++) {
+      if (i < halfwayPoint) {
+        digitStack.push(digitArray[i]);
+      } else if (i > halfwayPoint) {
+        if (digitStack.pop() != digitArray[i]) {
+          return false;
+        }
+      }
+    }
+  } else {
+    for(var i = 0; i < digitArray.length; i++) {
+      if (i < halfwayPoint) {
+        digitStack.push(digitArray[i]);
+      } else if (i >= halfwayPoint) {
+        if (digitStack.pop() != digitArray[i]) {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
 };
+
+console.log(isPalindrome(num1));
+console.log(isPalindrome(num2));
+console.log(isPalindrome(num3));
+console.log(isPalindrome(num4));
