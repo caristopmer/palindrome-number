f3fde66dc0fb18c960830beefbd987dc1594389f
caristopmer <chris.mcmenomy@gmail.com>
2017-12-18 11:40:14 -0600

    initial commit

---
+++ runner.js
@@ -0,0 +1,16 @@
+// Determine whether an integer is a palindrome. Do this without extra space.
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+
+var num1 = 1001;
+var num2 = 123882498;
+var num3 = 7823287;
+var num4 = 1233;
+
+
+var isPalindrome = function(x) {
+  
+};
